1c019040370130431dd12b5757575da8e4435950
asif-mohmd <asifasifpsps@gmail.com>
2024-03-09 19:19:01 +0530

    .env added and configured in github also

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+require("dotenv").config
 
 
 app.get("/api/get",(req,res)=>{
@@ -12,7 +13,8 @@ app.get("/api/get_user",(req,res)=>{
             name:"asif",
             age:22,
             contact:1234
-        }
+        },
+        env:process.env.NAME
     })
 })
 
@@ -21,6 +23,6 @@ app.get("/api/admin",(req,res)=>{
 })
 
 
-app.listen(8000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server started running on 8000 port")
 })
