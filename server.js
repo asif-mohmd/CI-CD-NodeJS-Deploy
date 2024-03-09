8d8f6d7d3e6ff3f18195a06e10a6f03ed90e9032
asif-mohmd <asifasifpsps@gmail.com>
2024-03-09 18:26:19 +0530

    user details added

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get("/api/get",(req,res)=>{
     res.send({message:"hello back to node js"})
 })
 
+app.get("/api/get_user",(req,res)=>{
+    res.send({
+        user:{
+            name:"asif",
+            age:22,
+            contact:1234
+        }
+    })
+})
+
 app.listen(8000,()=>{
     console.log("Server started running on 8000 port")
 })
